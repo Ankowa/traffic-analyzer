775c7ed0c21a365dc094b0ffa3f88ef8854b26ea
Ankowa <antonikowalczuk@gmail.com>
2017-01-28 15:02:18 +0100

    Pełen program do IP i MACów (Linux)
    Program działa dla każdej sieci i wypisuje w pliku IPs+MACs.txt w każdej linijce adres IP i MAC przyporządkowany każdemu urządzeniu użytkującemu sieć
    
    #Linux

---
+++ scan+list-finder-linux.py
@@ -0,0 +1,53 @@
+import os
+def prepare(text):
+	text=text.replace("(", "").replace(")", "")
+	return text
+
+def prepare_ip(text):
+	if(text[len(text)-1]!="."):
+		text=text[:-1]
+		prepare_ip(text)
+	text=text[:-3]
+	return text
+def find_ip_linux():
+	ifconfig=os.popen("ifconfig")
+	if_lines=ifconfig.readlines()
+	words=if_lines[1].split()
+	ip=words[1]
+	ip=prepare_ip(ip)
+	return ip
+def net_scan():
+	ip=find_ip_linux()
+	scan=os.popen("nmap -sP "+ip+".0/24")
+	scan_lines=scan.readlines()
+	output_file=open('scan.txt',"w")
+	for line in scan_lines:
+		output_file.write(line)
+	output_file.close()
+def adress_list():
+	net_scan()
+	input_file=open('scan.txt',"r+")
+	work_file=open('roboczy.txt',"w+")
+	output_file=open('IPs+MACs.txt', "w+")
+	in_lines=input_file.readlines()
+	input_file.close()
+	i=1
+	for line in in_lines:
+		if(i>2 and i%3!=1):
+			work_file.write(line)
+		i+=1
+	i=1
+	work_file.close()
+	work_file=open('roboczy.txt',"r+")
+	work_lines=work_file.readlines()
+	work_file.close()
+	for line in work_lines:	
+		words=line.split()
+		if(i%2==1):
+			output_file.write(prepare(words[-1])+"\t")
+		else:
+			output_file.write(prepare(words[2])+"\n")
+		i+=1
+	output_file.close()
+	os.remove('roboczy.txt')
+adress_list()
